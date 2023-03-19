957ca6b5ecc24a685ac45a76886108ed3ab705e2
Joanna Booth <118810624+stillawake17@users.noreply.github.com>
2023-03-19 20:55:02 +0000

    Updates

---
+++ precache-manifest.2b7378cb8413d6c6cef06c9de82cada4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "07c860ffd4a44ac244d2",
+    "url": "/ReactPortfolionpm/static/css/main.afab93f4.chunk.css"
+  },
+  {
+    "revision": "07c860ffd4a44ac244d2",
+    "url": "/ReactPortfolionpm/static/js/main.07c860ff.chunk.js"
+  },
+  {
+    "revision": "dccc1e22ce875cb129ea",
+    "url": "/ReactPortfolionpm/static/js/1.dccc1e22.chunk.js"
+  },
+  {
+    "revision": "21a3d205c37f7ff7d40a",
+    "url": "/ReactPortfolionpm/static/js/runtime~main.21a3d205.js"
+  },
+  {
+    "revision": "7760411a88f4d11ba32a1496d9d95ae6",
+    "url": "/ReactPortfolionpm/index.html"
+  }
+];
